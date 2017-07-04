7d802b7143a01e3dc083ea61dccf73121019c49b
Jonathan Serafini <jonathan.serafini@gmail.com>
2017-07-04 23:10:00 +0200

    or query

--- app/models/first.rb
+++ app/models/first.rb
@@ -1,4 +1,11 @@
 class First < ApplicationRecord
   has_many :seconds
   has_many :thirds
+
+
+  def self.full_join
+    self.joins(:seconds)
+        .joins(:thirds)
+        .where("thirds.name = ? OR seconds.name = ?", "b", "b")
+  end
 end
